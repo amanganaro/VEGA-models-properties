b3f247d9f3797d9cd2f8d5dda8070933f3169349
emagrifo <e.grifoni@kode-solutions.net>
2023-02-02 10:56:21 +0100

    add model EyeIrritation

--- VegaModels-EyeIrritation/src/main/java/insilico/eye_irritation/mainScriptEyeIrritation.java
+++ VegaModels-EyeIrritation/src/main/java/insilico/eye_irritation/mainScriptEyeIrritation.java
@@ -1,11 +1,14 @@
-package insilico.moa_epa;
+package insilico.eye_irritation;
 
+import insilico.core.exception.GenericFailureException;
+import insilico.core.exception.InitFailureException;
 import insilico.core.model.InsilicoModel;
 import insilico.core.model.InsilicoModelOutput;
+import insilico.core.molecule.InsilicoMolecule;
 import insilico.core.molecule.conversion.SmilesMolecule;
-//import lombok.extern.log4j.Log4j;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.openscience.cdk.io.formats.SMILESFIXFormat;
 import utils.ModelsDeployment;
 
 import java.io.File;
@@ -14,17 +17,16 @@ import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 import java.util.List;
 
+public class mainScriptEyeIrritation {
 
-public class mainScriptMoaEpa {
-    private static final Logger log = LogManager.getLogger(mainScriptMoaEpa.class);
+    private static final Logger log = LogManager.getLogger(mainScriptEyeIrritation.class);
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws InitFailureException, GenericFailureException {
 
-
-        InsilicoModel model = new ismMoaEpa();
+        ismEyeIrritation model = new ismEyeIrritation();
         ModelsDeployment.BuildDataset(model, "out_ts");
         File sourceFile = new File("out_ts/" + model.getInfo().getTrainingSetURL() + "/" + model.getInfo().getTrainingSetURL().split("/data/")[1]);
-        File destinationFile = new File("VegaModels-MoaEpa\\src\\main\\resources\\data\\ts_moa_epa.dat");
+        File destinationFile = new File("VegaModels-EyeIrritation\\src\\main\\resources\\data\\ts_eye_irritation.dat");
         try {
             Files.move(sourceFile.toPath(), destinationFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
         } catch (Exception ex) {
@@ -36,25 +38,21 @@ public class mainScriptMoaEpa {
         smilesList.add("O=S(=O)(N)c1cc2c(cc1C(F)(F)F)NC(NS2(=O)(=O))Cc3ccccc3");
         smilesList.add("O=[N+]([O-])c1cc(c(O)c(c1)C(C)(C)C)[N+](=O)[O-]");
         smilesList.add("O=S(=O)(C(C)(C)C)C(C)(C)C");
-        smilesList.add("O=C(OC2CC1N(C)C(CC1)C2(C(=O)OC))c3ccccc3");
+        smilesList.add("O=[N+]([O-])c1cnc(n1C)C(C)C");
+        smilesList.add("n1ccn(c1)C(c2ccccc2)(c3ccccc3)c4ccccc4Cl");
+        smilesList.add("O=[N+]([O-])c1ccc(cc1)Sc2ccc(cc2)[N+](=O)[O-]");
+        smilesList.add("O=Cc1cc(ccc1(O))Cl");
 
 
-        for(String smiles : smilesList) {
+        for (String smiles : smilesList) {
             InsilicoModelOutput out = model.Execute(SmilesMolecule.Convert(smiles));
-            for(int i = 0; i < model.GetResultsName().length; i++)
+            for (int i = 0; i < model.GetResultsName().length; i++)
                 System.out.println(model.GetResultsName()[i] + " | " + out.getResults()[i]);
         }
-//        for(String smiles : smilesList) {
-//            InsilicoModelOutput out = model.Execute(SmilesMolecule.Convert(smiles));
-//            for(int i = 0; i < model.getDescriptorsSize(); i++){
-//                System.out.println(model.getDescriptorsNames()[i] + " === " + model.GetDescriptor(i));
-//
-//            }
-//            System.out.println("==============");
-//
-//        }
 
+//        PERFORMANCES:
+//        Training:
+//        Test:
 
     }
-
 }
