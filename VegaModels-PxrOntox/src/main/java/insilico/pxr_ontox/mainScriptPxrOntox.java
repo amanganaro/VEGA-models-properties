277e1f1aa43bf6ccce5fe1874f0cd46269804bb3
davide <dpirolo@protoqsar.com>
2025-05-30 10:24:01 +0200

    added new model: pxr ontox

---
+++ VegaModels-PxrOntox/src/main/java/insilico/pxr_ontox/mainScriptPxrOntox.java
@@ -0,0 +1,57 @@
+package insilico.pxr_ontox;
+
+import insilico.core.exception.GenericFailureException;
+import insilico.core.exception.InitFailureException;
+import insilico.core.model.InsilicoModel;
+import insilico.core.model.InsilicoModelOutput;
+import insilico.core.model.InsilicoModelPython;
+import insilico.core.molecule.conversion.SmilesMolecule;
+import insilico.core.python.CdddDescriptors;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class mainScriptPxrOntox {
+    public static void main(String[] args) throws InitFailureException, GenericFailureException, IOException {
+        InsilicoModel model = new ismPxrOntox(true, null);
+
+//
+//        ModelsDeployment.BuildDataset(model, "out_ts");
+//        File sourceFile = new File("out_ts/" + model.getInfo().getTrainingSetURL() + "/" + model.getInfo().getTrainingSetURL().split("/data/")[1]);
+//        File destinationFile = new File("VegaModels-ApicalCardioTox\\src\\main\\resources\\data\\ts_apical_cardio_tox.dat");
+//        try {
+//            Files.move(sourceFile.toPath(), destinationFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+//        } catch (Exception ex) {
+//            log.warn(ex.getMessage());
+//        }
+//        if(1==1)
+//            return;
+        model.setSkipADandTSLoading(true);
+
+        List<String> smilesList = new ArrayList<>();
+        smilesList.add("O=[N+]([O-])c1cc(cc(c1N(CCC)CCC)[N+](=O)[O-])S(=O)(=O)C");
+        smilesList.add("O=S(=O)(N)c1cc2c(cc1C(F)(F)F)NC(NS2(=O)(=O))Cc3ccccc3");
+        smilesList.add("O=[N+]([O-])c1cc(c(O)c(c1)C(C)(C)C)[N+](=O)[O-]");
+        smilesList.add("O=S(=O)(C(C)(C)C)C(C)(C)C");
+
+        CdddDescriptors cdddDescriptors=null;
+
+        if(InsilicoModelPython.class.isAssignableFrom(model.getClass())){
+            cdddDescriptors = new CdddDescriptors(smilesList, true, null);
+            ((ismPxrOntox) model).setDescriptorGenerator(cdddDescriptors);
+            boolean descriptorOK = cdddDescriptors.calculateDescriptors();
+        }
+
+        for (String smiles : smilesList) {
+            InsilicoModelOutput out = model.Execute(SmilesMolecule.Convert(smiles));
+            System.out.println("\n"+smiles);
+            for (int i = 0; i < model.GetResultsName().length; i++)
+                System.out.println(model.GetResultsName()[i] + " | " + out.getResults()[i]);
+            for (int i = 0; i < model.GetADItemsName().length; i++)
+                System.out.println(model.GetADItemsName()[i] + " | " + out.getADIndex().get(i).GetIndexValueFormatted() );
+        }
+
+        cdddDescriptors.dispose();
+    }
+}
